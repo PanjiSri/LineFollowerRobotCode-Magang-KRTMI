265d2b6bec342bfa4b4ebac39bf3c8ce2c2696b8
muhhul <fatihulkuliah@gmail.com>
2023-04-09 11:31:03 +0700

    update kecepatan dan posisi

--- linefollower.ino
+++ linefollower.ino
@@ -1,3 +1,6 @@
+//Import library
+#include <math.h>
+
 //Pendefinisian nomor pin untuk dihibungkan dengan pin OUT pada IR sensor
 #define IR_Sensor1 /*No Pin*/  
 #define IR_Sensor2 /*No Pin*/  
@@ -19,14 +22,23 @@ int IR2;
 int IR3;
 
 //Pendefinisian nomor pin untuk encoder
-#define encoderA /*No Pin*/
-#define encoderB /*No Pin*/
+#define encoder1A /*No Pin*/
+#define encoder1B /*No Pin*/
+#define encoder2A /*No Pin*/
+#define encoder2B /*No Pin*/
 
 //Deklarasi untuk encoder
-long encoderPosition = 0;
+long encoder1Position = 0;
+long encoder2Position = 0;
 float rpm,speed;
 int interval=100;
 unsigned int time,newTime;
+
+//Deklarasi untuk position
+float positionX = 0;
+float positionY = 0;
+float degrees = 0;
+float newDegrees;
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,9 +46,12 @@ void setup() {
   pinMode(IR_Sensor2, INPUT); 
   pinMode(IR_Sensor3, INPUT);
 
-  pinMode(encoderA, INPUT);
-  pinMode(encoderB, INPUT);
-  attachInterrupt(/*No Pin Encoder*/, triggerEncoder, CHANGE);
+  pinMode(encoder1A, INPUT);
+  pinMode(encoder1B, INPUT);
+  pinMode(encoder2A, INPUT);
+  pinMode(encoder2B, INPUT);
+  attachInterrupt(/*No Pin Encoder*/, trigger1Encoder, CHANGE);
+  attachInterrupt(/*No Pin Encoder*/, trigger2Encoder, CHANGE);
   Serial.begin (9600);
 }
 
@@ -77,18 +92,40 @@ void loop() {
 
   newtime=millis();
   if (newTime - time >= interval){
-    rpm = (encoderPosition/30.00)/0.001666667;
-    speed = (2*3.14*rpm)/60
+    //Program mencari kecepatan robot
+    rpm1 = (encoder1Position/330)/0.001666667;
+    speed1 = (2 * 3.14 * rpm1 * r/*Jari jari roda*/)/60;
+    rpm2 = (encoder2Position/330)/0.001666667;
+    speed2 = (2 * 3.14 * rpm2 * r/*Jari jari roda*/)/60;
+    speed = (speed1 + speed2)/2;
     time = newTime;
-    encoderPosition = 0;
+    encoder1Position = 0;
+    encoder2Position = 0;
+
+    //Mengetahui sudut robot
+    newDegrees = degrees + (/*wheel radius*/ * (speed2 - speed1)/(2 * /*jarak roda ke center*/))
+    degrees = newDegrees
+
+    //Posisi terhadap X
+    positionX = positionX + speed * cos(degrees)
+    //Posisi terhadap Y
+    positionY = positionY + speed * sin(degrees)
   }
 
 }
 
-void triggerEncoder(){
-  if (digitalRead(encoderA)==digitalRead(encoderB)){
-    encoderPosition--;
+void trigger1Encoder(){
+  if (digitalRead(encoder1A)==digitalRead(encoder1B)){
+    encoder1Position--;
   }else{
-    encoderPosition++;
+    encoder1Position++;
+  }
+}
+
+void trigger2Encoder(){
+  if (digitalRead(encoder2A)==digitalRead(encoder2B)){
+    encoder2Position--;
+  }else{
+    encoder2Position++;
   }
 }
